f05ac5b629cc12e5534af28b3cddc65270858c7c
niu <123456@163.com>
2025-06-04 22:45:46 +0800

    new start

---
+++ app/src/main/java/com/example/a2048/StartActivity.java
@@ -0,0 +1,26 @@
+package com.example.a2048;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button startButton = findViewById(R.id.button_start_game);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
